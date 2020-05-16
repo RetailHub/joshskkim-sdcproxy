70b71d4891a8190a7d18c3ab36718a176b38f090
root <marcusbarnesdev@gmail.com>
2020-05-15 21:01:34 -0700

    fix a few thangs

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 const {createProxyMiddleware} = require('http-proxy-middleware');
-const port = 4001;
+const port = 80;
 const path = require('path');
 
-const apiProxy = createProxyMiddleware({target: 'http://localhost:3001'});
-const reviewProxy = createProxyMiddleware({target: 'http://localhost:3004'});
-const relatedProxy = createProxyMiddleware({target: 'http://localhost:3003'});
-const itemsProxy = createProxyMiddleware({target: 'http://localhost:3002'});
+const apiProxy = createProxyMiddleware({target: 'http://ec2-3-132-5-204.us-east-2.compute.amazonaws.com:3001'});
+const reviewProxy = createProxyMiddleware({target: 'http://18.212.184.37:3004'});
+const relatedProxy = createProxyMiddleware({target: 'http://54.89.234.183:3003'});
+const itemsProxy = createProxyMiddleware({target: 'http://34.201.53.74:3002'});
 app.use(express.static('public'))
 
 app.use('/api/items/:id',apiProxy);

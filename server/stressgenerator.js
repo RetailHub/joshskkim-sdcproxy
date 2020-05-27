8afc1e18e5b6cc99fae23e033d5dc1c0910899fe
joshskkim <josh.sk.kim@gmail.com>
2020-05-27 10:04:59 -0700

    Create random generator for stress tester

---
+++ server/stressgenerator.js
@@ -0,0 +1,22 @@
+const faker = require('faker');
+
+function generateRandomData(userContext, events, done) {
+  const id = faker.random.number(10000000);
+  const itemId = faker.random.number(11000000, 12000000);
+  const arrayLength = faker.random.number(1, 5);
+  const altImages = [];
+
+  for (let i = 0; i < arrayLength; i += 1) {
+    const imageIndex = faker.random.number(20);
+    altImages.push(`'https://sdcimages.s3-us-west-1.amazonaws.com/img${imageIndex}.jpg'`);
+  }
+
+  userContext.vars.id = id;
+  userContext.vars.itemId = itemId;
+  userContext.vars.altImages = altImages;
+  return done();
+}
+
+module.exports = {
+  generateRandomData,
+};
